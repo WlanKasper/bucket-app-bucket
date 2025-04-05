01a0d3cecba7e0b2dea0169d31140fb4146045df
Artur Smirnov <artur-smirnov@dedagroup.it>
2025-04-05 22:33:23 +0200

    wip

--- src/helpers/urlEncode.ts
+++
@@ -1,5 +0,0 @@
-export const URLEncode = (str: string) => {
-  return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
-};
-
-export default URLEncode;
--- src/helpers/uuid.ts
+++
@@ -1,5 +0,0 @@
-import uuid from "react-native-uuid";
-
-export const generateShortUUID = () => {
-  return uuid.v4();
-};
