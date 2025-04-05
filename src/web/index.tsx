d3b1e9ce00b2307186e20a9ed806b5e4840c65f5
Artur Smirnov <artur-smirnov@dedagroup.it>
2025-04-05 22:48:08 +0200

    wip

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { theme } from "@/design/theme";
+import { theme } from "@/theme/theme";
 import { Provider } from "react-redux";
 import { store } from "@/store/configureStore";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import dotenv from 'dotenv';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 dotenv.config();
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   define: {
-    'process.env': process.env
-  }
+    "process.env": process.env,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
