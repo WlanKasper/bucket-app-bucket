95609ba072b5d43ff98a8db99468bd2352737572
Artur Smirnov <artur-smirnov@dedagroup.it>
2025-04-06 17:44:07 +0200

    test

--- src/helpers/telegram/utils.ts
+++ src/helpers/telegram/utils.ts
@@ -1,7 +1,10 @@
 import TMA from "@twa-dev/sdk";
+import { useEffect, useState } from "react";
+import viewport from "@twa-dev/sdk";
 
 export const initApp = () => {
   TMA.ready();
+  TMA.expand();
   TMA.requestFullscreen();
 };
 
@@ -11,3 +14,19 @@ export const getTelegramUser = () =>
     first_name: "Peter",
     photo_url: "",
   };
+
+export const useSafeAreaInsets = () => {
+  const [insets, setInsets] = useState(TMA.safeAreaInset);
+
+  useEffect(() => {
+    const handleChange = () => setInsets(TMA.safeAreaInset);
+
+    viewport.onEvent("safeAreaChanged", handleChange);
+
+    return () => {
+      viewport.offEvent("safeAreaChanged", handleChange);
+    };
+  }, []);
+
+  return insets;
+};
--- src/web/App.tsx
+++ src/web/App.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import { useDispatch } from "react-redux";
-import { bucketActions, bucketReducer } from "@/store/bucket";
+import { bucketActions } from "@/store/bucket";
 import { getTelegramUser, initApp } from "@/helpers/telegram/utils";
+import SafeAreaWrapper from "./home/SafeAreaWrapper";
 
 const App = () => {
   initApp();
@@ -17,9 +18,11 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <SafeAreaWrapper>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </SafeAreaWrapper>
   );
 };
 
